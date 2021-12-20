b46e5e7bd4f7845390f29b28de0c18a3b9924f0e
Lanscarlos <lanscarlos@hotmail.com>
2021-12-20 17:01:12 +0800

    update ranales kether

--- src/main/kotlin/top/lanscarlos/ranales/kether/ActionRanales.kt
+++ src/main/kotlin/top/lanscarlos/ranales/kether/ActionRanales.kt
@@ -1,12 +1,10 @@
 package top.lanscarlos.ranales.kether
 
-import taboolib.common.platform.function.adaptCommandSender
 import taboolib.module.kether.*
-import java.util.concurrent.CompletableFuture
 
 object ActionRanales {
 
-    @KetherParser(["ranales"], namespace = "ranales", shared = true)
+    @KetherParser(["ranales", "rl"], namespace = "ranales", shared = true)
     fun parser() = scriptParser {
         it.switch {
             case("script") { ActionScript.parse(it) }
