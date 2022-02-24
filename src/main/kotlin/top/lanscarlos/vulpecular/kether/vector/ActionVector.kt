6e48b9d46f2f26f4360a54192aa9eaece42bfdc6
Lanscarlos <lanscarlos@hotmail.com>
2022-02-24 11:28:05 +0800

    update

---
+++ src/main/kotlin/top/lanscarlos/vulpecular/kether/vector/ActionVector.kt
@@ -0,0 +1,31 @@
+package top.lanscarlos.vulpecular.kether.vector
+
+import taboolib.library.kether.ArgTypes
+import taboolib.module.kether.KetherParser
+import taboolib.module.kether.scriptParser
+import top.lanscarlos.vulpecular.kether.entity.ActionEntity
+
+/**
+ * Vulpecular
+ * top.lanscarlos.vulpecular.kether.vector
+ *
+ * @author Lanscarlos
+ * @since 2022-02-23 22:16
+ */
+object ActionVector {
+
+
+
+    @KetherParser(["vector"], namespace = "vulpecular", shared = true)
+    fun ketherParser() = scriptParser {
+        it.mark()
+        var token = it.nextToken()
+        val entity = if (!ActionEntity.isAction(token)) {
+            it.reset()
+            it.next(ArgTypes.ACTION).apply {
+                token = it.nextToken()
+            }
+        } else null
+        ActionEntity.getAction(token)?.resolve(it, entity) ?: error("Unknown type \"$token\" of entity action")
+    }
+}
